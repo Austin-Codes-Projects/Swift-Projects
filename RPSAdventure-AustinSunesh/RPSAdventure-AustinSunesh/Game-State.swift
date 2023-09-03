b979eee6f7ae0ad15ca507e2c8e015fe5a5bed01
Austin-Codes-Projects <143961270+Austin-Codes-Projects@users.noreply.github.com>
2023-09-03 15:34:14 -0400

    Add files via upload

---
+++ RPSAdventure-AustinSunesh/RPSAdventure-AustinSunesh/Game-State.swift
@@ -0,0 +1,28 @@
+//
+//  Game-State.swift
+//  RPSAdventure-AustinSunesh
+//
+//  Created by Sunesh, Austin on 2023-04-28.
+//
+
+import Foundation
+
+enum GameState {
+    case start
+    case win
+    case lose
+    case draw
+    
+    var status: String {
+        switch self {
+        case .start:
+            return "Rock, Paper, Scissors?"
+        case .win:
+            return "You Won!"
+        case .lose:
+            return "You Lost!"
+        case .draw:
+            return "It's a Draw!"
+        }
+    }
+}
