bc0174dc7f178425951db02f825ac36138c20499
Austin-Codes-Projects <143961270+Austin-Codes-Projects@users.noreply.github.com>
2023-09-03 14:58:25 -0400

    Add files via upload
    Here is my first time creating an animation in Swift

---
+++ BoogieBotApp/BoogieBotApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BoogieBotApp
+//
+//  Created by Sunesh, Austin on 2023-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
